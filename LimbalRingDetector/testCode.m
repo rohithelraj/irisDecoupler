4e7ecc3f0fc87307352c7f906a5d15476fcb4776
ifigast <rohith.el.raj@gmail.com>
2018-05-05 12:44:56 +0200

    Color Based Segmentation, Test data and generation code added. Forked from https://de.mathworks.com/help/images/examples/color-based-segmentation-using-k-means-clustering.html.

---
+++ LimbalRingDetector/testCode.m
@@ -0,0 +1,23 @@
+%Source - Matlab : https://de.mathworks.com/help/images/examples/color-based-segmentation-using-k-means-clustering.html
+lab_iris_actual_nopupil = rgb2lab(iris_actual_nopupil);
+ab = lab_iris_actual_nopupil(:,:,2:3);
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+nColors = 3;
+[cluster_idx, cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+figure,imshow(pixel_labels,[]), title('image labeled by cluster index');
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+
+for k = 1:nColors
+    color = iris_actual_nopupil;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+end
+
+figure,imshow(segmented_images{1}), title('objects in cluster 1');
+figure,imshow(segmented_images{2}), title('objects in cluster 2');
+figure,imshow(segmented_images{3}), title('objects in cluster 3');
+                                  
