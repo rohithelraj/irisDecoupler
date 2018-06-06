30064642770f2f68c36941a93609f4657d8edbaa
ifigast <rohith.el.raj@gmail.com>
2018-06-06 14:02:29 +0200

    Project reorganization.

--- LimbalRingDetector/cannyEdgeDetector_forEye.m
+++
@@ -1,8 +0,0 @@
-function cannyImageOutput = cannyEdgeDetector_forEye(image)
-%CANNYEDGEDETECTOR_FORPUPIL Summary of this function goes here
-%   Detailed explanation goes here
-gray = rgb2gray(image);
-cannyImageOutput = edge(gray,'Canny');
-
-end
-
--- LimbalRingDetector/findPupil.m
+++
@@ -1,9 +0,0 @@
-function [all_blobs,pupil] = findPupil( whole_eye )
-%FINDPUPIL Summary of this function goes here
-%   Detailed explanation goes here
-grey = imono(whole_eye);
-pupil = grey > 0;
-all_blobs = iblobs(pupil,'boundary');
-
-
-
