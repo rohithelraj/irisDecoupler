0932460d0462382b7c15fb2661212d04f4223319
ifigast <rohith.el.raj@gmail.com>
2018-07-06 15:09:16 +0200

    Hue based histogram Renge finder implemented.

--- LimbalRingDetector/CollaretteDetector.m
+++ LimbalRingDetector/CollaretteDetector.m
@@ -1,33 +1,14 @@
-[iris_actual_nopupil,img_name] = imageDataLoader('with Limbal Ring',7);
+[iris_actual_nopupil,img_name] = imageDataLoader('no Limbal Ring',1);
 %This detection works only with green colored eyes.
 result = iris_actual_nopupil;
 lab_iris_actual_nopupil = rgb2lab(iris_actual_nopupil);
 hsv = rgb2hsv(lab_iris_actual_nopupil);
 %generic implementation for all colored eyes.
-rounded_hsv = round(hsv(:,:,1),4);
-[counts,binLocations]  = imhist(rounded_hsv,1000);
-
-counter_histo = 1; 
-counter_zero = 1; 
-new_binLocations(1) = 0;
-zero_locations(1) = 0;
-new_counts(1) = 0;
-ranges = 0;
-for x = 1:size(counts)
-     if(counts(x) == 0)
-         zero_locations(counter_zero) = binLocations(x);
-         counter_zero = counter_zero + 1;  
-     end
-     if(counts(x) > 500)
-       new_binLocations(counter_histo) = binLocations(x); 
-       new_counts(counter_histo) = counts(x);
-       counter_histo = counter_histo + 1;  
-     end     
-end
-
-[M,I] = max(new_counts);
-Peak = new_binLocations(I);
-
+rounded_hsv = round(hsv(:,:,1),5);
+[hsvCounts,hsvValues]  = imhist(rounded_hsv,1000);
+[RangeHSV_min,RangeHSV_max,CountHSV_min,CountHSV_max] = hSVRangeFinder(hsvCounts,hsvValues);
+[RangeWiseCount] = hSVRangeWiseCountFinder(RangeHSV_min,RangeHSV_max,hsvCounts,hsvValues);
+[BestHSVRange_min,BestHSVRange_max,BestRangeWiseCount] = hSVRangeFilter(RangeWiseCount,RangeHSV_min,RangeHSV_max,100);
 [rows,columns,noChannels] = size(hsv);
     for x = 1:rows
         for y = 1:columns
@@ -36,12 +17,11 @@ Peak = new_binLocations(I);
                 result(x,y,2) = 0;
                 result(x,y,3) = 0;
             end
-            if hsv(x,y,1) == Peak
+            if~((hsv(x,y,1) > BestHSVRange_min(1))&&(rounded_hsv(x,y) < BestHSVRange_max(1)))
                 result(x,y,1) = 0;
                 result(x,y,2) = 0;
-                result(x,y,3) = 0;
+                result(x,y,3) = 0;            
             end
-            
-        end
+        end 
     end
   idisp({result,iris_actual_nopupil});  
---
+++ LimbalRingDetector/hSVRangeFilter.m
@@ -0,0 +1,22 @@
+function [BestHSVRange_min,BestHSVRange_max,BestRangeWiseCount] = hSVRangeFilter(RangeWiseCount,RangeHSV_min,RangeHSV_max,countThreshold)
+%HSVRANGEFILTER Summary of this function goes here
+%   Filers the best ranges that havve the possibilies to be a collarette
+%   section.
+    counter_range = 1;
+    BestHSVRange_min(1) = 0.0000;
+    BestHSVRange_max(1) = 0.0000;
+    BestRangeWiseCount(1) = 0;
+    Range_size = size(RangeHSV_min);
+    
+    for i = 1:Range_size(2)
+        %fprintf('\n in with: %d',RangeWiseCount(i));
+        if(RangeWiseCount(i) > countThreshold)
+            %fprintf('\n out with: %d at counter : %d',RangeWiseCount(i),counter_range);
+            BestHSVRange_min(counter_range) = RangeHSV_min(i);
+            BestHSVRange_max(counter_range) = RangeHSV_max(i);
+            BestRangeWiseCount(counter_range) = RangeWiseCount(i);
+            counter_range = counter_range + 1;
+        end
+    end
+end
+
