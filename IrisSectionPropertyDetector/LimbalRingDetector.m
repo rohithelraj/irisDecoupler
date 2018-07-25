0a67185d46389b680968a4761612c2261bce706d
ifigast <rohith.el.raj@gmail.com>
2018-07-25 13:30:21 +0200

    A section for computing logistic regression has been added to the property detector part.

--- IrisSectionPropertyDetector/LimbalRingDetector.m
+++ IrisSectionPropertyDetector/LimbalRingDetector.m
@@ -1,4 +1,4 @@
-[iris_actual_nopupil, section_limbalRing, section_flecks, section_collarette, section_stroma, image_name] = imageDataLoader('no collarette with limbal ring',6);
+[iris_actual_nopupil, section_limbalRing, section_flecks, section_collarette, section_stroma, image_name] = imageDataLoader('no collarette with limbal ring',7);
 %Source - Matlab : https://de.mathworks.com/help/images/examples/color-based-segmentation-using-k-means-clustering.html
 lab_iris_actual_nopupil = rgb2lab(section_limbalRing);
 ab = lab_iris_actual_nopupil(:,:,2:3);
@@ -12,18 +12,14 @@ pixel_labels = reshape(cluster_idx,nrows,ncols);
 % figure,imshow(pixel_labels,[]), title('image labeled by cluster index');
 segmented_images = cell(1,3);
 rgb_label = repmat(pixel_labels,[1 1 3]);
-
 for k = 1:nColors
     color = section_limbalRing;
     color(rgb_label ~= k) = 0;
     segmented_images{k} = color;
 end
-
 figure,imshow(segmented_images{1});
 figure,imshow(segmented_images{2});
 figure,imshow(segmented_images{3});
-
-
 % figure,imshow(segmented_images{4}), title('objects in cluster 4');  
 % figure,imshow(segmented_images{5}), title('objects in cluster 5');
 % figure,imshow(segmented_images{6}), title('objects in cluster 6');
