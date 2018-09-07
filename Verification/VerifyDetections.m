9c17e4ae619b02c4aa34f48ea91b1dcaa2b620b9
Rohith <rohith.el.raj@gmail.com>
2018-09-07 15:16:30 +0200

    Updating with actual data.

--- Verification/VerifyDetections.m
+++ Verification/VerifyDetections.m
@@ -1,9 +1,35 @@
-LimbalRing_flag = 0;
-Collarette_flag = 0;
-Flecks_flag = 0;
-physical_anisocoria_flag = 0;
-sphincter_contrast_flag = 0;
-Detections = strings(10,1);
+
+Detections = strings(20,1);
 count = 1;
-Detections(count,1) = 'GG at RS12906280';
+Detections(count,1) = 'AC at RS4778241';
 count = count + 1;
+Detections(count,1) = 'AG at RS4778138';
+count = count + 1;
+Detections(count,1) = 'CC at RS3794604';
+count = count + 1;
+Detections(count,1) = 'CT at RS12203592';
+count = count + 1;
+Detections(count,1) = 'CT at RS1800407';
+count = count + 1;
+Detections(count,1) = 'CT at RS4553604';
+count = count + 1;
+Detections(count,1) = 'CT at RS1129038';
+count = count + 1;
+Detections(count,1) = 'AA at RS1105879';
+count = count + 1;
+Detections(count,1) = 'CG at RS16891982';
+count = count + 1;
+Detections(count,1) = 'CT at RS39 35591';
+count = count + 1;
+Detections(count,1) = 'CT at RS1667394';
+count = count + 1;
+Detections(count,1) = 'CT at RS91 6977';
+count = count + 1;
+Detections(count,1) = 'AG at RS7174027';
+count = count + 1;
+Detections(count,1) = 'AG at RS7495174';
+
+
+
+
+
