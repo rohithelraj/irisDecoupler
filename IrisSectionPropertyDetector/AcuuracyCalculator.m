a4bb2b30171e32ea3acce9a77a4d6ccb4aae9089
Rohith <rohith.el.raj@gmail.com>
2018-09-14 15:49:37 +0200

    some updations.

--- IrisSectionPropertyDetector/AcuuracyCalculator.m
+++ IrisSectionPropertyDetector/AcuuracyCalculator.m
@@ -25,6 +25,25 @@ function accuracy = AcuuracyCalculator(selectedRGB, comparedRGB, type)
                     end
                 end    
             end
+        case 'limbalRing'
+            difference = abs((transpose(selectedRGB))- comparedRGB);
+            differ_elements = find(difference > 26);
+            
+            if (abs(sum (selectedRGB) - sum(comparedRGB))) > 70
+                accuracy = 100;
+            elseif(length(differ_elements) > 1)
+                accuracy = 90;
+            elseif(length(differ_elements) == 1)
+                accuracy = 50;
+            elseif(selectedRGB(1) > selectedRGB(2))
+                if(selectedRGB(2) > selectedRGB(3))
+                    
+                    if((abs(selectedRGB(1) - selectedRGB(2))) > 20)&&((abs(selectedRGB(2) - selectedRGB(1))) >20)
+                        %Brown color detected.
+                        accuracy = 50;
+                    end
+                end    
+            end            
         otherwise
             sprintf('Invalid type value in AcuuracyCalculator-> %s'+type);
     end
